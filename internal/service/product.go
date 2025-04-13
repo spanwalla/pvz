0260c4a0f1822414c481614f80a4a39f4442cb42
spanwalla <lus550@yandex.ru>
2025-04-14 01:59:13 +0300

    fixes in workflows and linter

--- internal/service/product.go
+++ internal/service/product.go
@@ -1,54 +1,54 @@
 package service
 
 import (
-    "context"
-    "errors"
+	"context"
+	"errors"
 
-    "github.com/google/uuid"
-    log "github.com/sirupsen/logrus"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
 
-    "github.com/spanwalla/pvz/internal/entity"
-    "github.com/spanwalla/pvz/internal/metrics"
-    "github.com/spanwalla/pvz/internal/repository"
+	"github.com/spanwalla/pvz/internal/entity"
+	"github.com/spanwalla/pvz/internal/metrics"
+	"github.com/spanwalla/pvz/internal/repository"
 )
 
 var (
-    ErrCannotCreateProduct = errors.New("cannot create product")
+	ErrCannotCreateProduct = errors.New("cannot create product")
 )
 
 type ProductService struct {
-    productRepo     repository.Product
-    receptionRepo   repository.Reception
-    productsCreated metrics.Counter
+	productRepo     repository.Product
+	receptionRepo   repository.Reception
+	productsCreated metrics.Counter
 }
 
 func NewProductService(productRepo repository.Product, receptionRepo repository.Reception, productsCreated metrics.Counter) *ProductService {
-    return &ProductService{
-        productRepo:     productRepo,
-        receptionRepo:   receptionRepo,
-        productsCreated: productsCreated,
-    }
+	return &ProductService{
+		productRepo:     productRepo,
+		receptionRepo:   receptionRepo,
+		productsCreated: productsCreated,
+	}
 }
 
 func (s *ProductService) Create(ctx context.Context, pointID uuid.UUID, productType entity.ProductType) (entity.Product, error) {
-    receptionID, err := s.receptionRepo.GetActiveID(ctx, pointID)
-    if err != nil {
-        if errors.Is(err, repository.ErrNotFound) {
-            return entity.Product{}, ErrActiveReceptionNotFound
-        }
+	receptionID, err := s.receptionRepo.GetActiveID(ctx, pointID)
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			return entity.Product{}, ErrActiveReceptionNotFound
+		}
 
-        log.Errorf("ProductService.Create - s.receptionRepo.GetActiveID: %v", err)
-        return entity.Product{}, ErrCannotCreateProduct
-    }
+		log.Errorf("ProductService.Create - s.receptionRepo.GetActiveID: %v", err)
+		return entity.Product{}, ErrCannotCreateProduct
+	}
 
-    log.Debugf("ProductService.Create - receptionID: %v", receptionID)
+	log.Debugf("ProductService.Create - receptionID: %v", receptionID)
 
-    product, err := s.productRepo.Create(ctx, receptionID, productType)
-    if err != nil {
-        log.Errorf("ProductService.Create - s.productRepo.Create: %v", err)
-        return entity.Product{}, ErrCannotCreateProduct
-    }
+	product, err := s.productRepo.Create(ctx, receptionID, productType)
+	if err != nil {
+		log.Errorf("ProductService.Create - s.productRepo.Create: %v", err)
+		return entity.Product{}, ErrCannotCreateProduct
+	}
 
-    s.productsCreated.Inc()
-    return product, nil
+	s.productsCreated.Inc()
+	return product, nil
 }
